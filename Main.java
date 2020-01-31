da2f10772407b67cdbd991f540323f63808dcbf8
Eduard Gradwald <e.gradwald@gmail.com>
2020-01-31 08:53:29 +0100

    Test commit

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		BallDemo b = new BallDemo(10);
 		b.springenLassen();
+
 	}
 
 }
---
+++ Test.java
@@ -0,0 +1,7 @@
+/**
+ * -------------------------- Created with IntelliJ IDEA. --------------------------- User:
+ * Eduard Gradald Class:   11B392 Date: Time: ---------------------------
+ */
+public class Test {
+
+}
